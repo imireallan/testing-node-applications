90014e87cdc63824d2821a49d4f345e825677f1d
Allan Imire <allanimire@gmail.com>
2023-10-31 20:05:25 +0300

    add custom command for login

--- cypress/e2e/loginpage.cy.js
+++ cypress/e2e/loginpage.cy.js
@@ -1,20 +1,14 @@
 describe("The login process", () => {
   it("Should login user, flash message and redirect to homepage", () => {
-    cy.intercept({
-        method: 'POST',
-        url: '/api/v1/auth/login',
-      }, { fixture: 'user.json' })
+    cy.login("test@user4.com", "pass@word1");
 
-    cy.visit("/auth/login")
-    cy.get("input[name='email']").type('test@user4.com')
-    cy.get("input[name='password']").type('pass@word1')
+    cy.get("button").click();
 
-    cy.get('button').click()
-
-    cy.get('#email-confirm').should('contain', 'Please confirm your email address.')
-    cy.get('#auth-username').should('contain', 'Hey, Test User4')
-    cy.get('#auth-logout').should('contain', 'Logout')
-
+    cy.get("#email-confirm").should(
+      "contain",
+      "Please confirm your email address."
+    );
+    cy.get("#auth-username").should("contain", "Hey, Test User4");
+    cy.get("#auth-logout").should("contain", "Logout");
   });
 });
- 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,7 +10,19 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("login", (email, password) => {
+  cy.intercept(
+    {
+      method: "POST",
+      url: "/api/v1/auth/login",
+    },
+    { fixture: "user.json" }
+  );
+
+  cy.visit("/auth/login");
+  cy.get("input[name='email']").type(email);
+  cy.get("input[name='password']").type(password);
+});
 //
 //
 // -- This is a child command --
