436b97aac5b77e7bdf810e1aa423cecd9fd9563e
Allan Imire <allanimire@gmail.com>
2023-10-31 13:38:18 +0300

    add tests for login validator

---
+++ __tests__/server/validators/login.spec.js
@@ -0,0 +1,51 @@
+import loginValidator from "@validators/login";
+
+describe("The login validator", () => {
+  it("Should call the next function when validation succeeds", async () => {
+    const req = {
+      body: {
+        email: "a@gmail.com",
+        password: "pass@word1",
+      },
+    };
+    const res = {};
+    const next = jest.fn();
+
+    await loginValidator(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+  });
+  it("Should return 422 when validation fails", async () => {
+    class Response {
+      status(status) {
+        this.status = status;
+        return this;
+      }
+      json(data) {
+        this.data = data;
+      }
+    }
+    const req = {
+      body: {
+        password: "pass@word1",
+      },
+    };
+
+    const next = jest.fn();
+    const res = new Response();
+    const statusSpy = jest.spyOn(res, "status");
+    const jsonSpy = jest.spyOn(res, "json");
+
+    await loginValidator(req, res, next);
+
+    expect(statusSpy).toHaveBeenCalledWith(422);
+    expect(jsonSpy).toHaveBeenCalledWith({
+      message: "Validation failed.",
+      data: {
+        errors: {
+          email: "email is a required field",
+        },
+      },
+    });
+  });
+});
